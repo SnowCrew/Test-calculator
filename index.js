bc63e96cd7ef7ce119e2855a3a91839e49a09a19
SlowBurn <VanhelsingX3000@gmail.com>
2022-09-12 00:02:30 +0300

    Add calc helper and fixing bugs

--- index.js
+++ index.js
@@ -14,18 +14,41 @@ let lastOperationNumber = '';
 let nextOperationNumber = '';
 let finish = false;
 
-//Calc button's keys array
-//digit key-values in buttons [".","0",...,"9"]
+//Calc button's keys arrays
+//Elements digit key-values in buttons [".","0",...,"9"]
 let digitKeys = [];
 document.querySelectorAll('.digits').forEach(el => { digitKeys.push(el.textContent) })
-//Math operations key-values in buttons ["+", "-", "/", "*", "%"]
+//Elements math operations key-values in buttons ["+", "-", "/", "*", "%"]
 let mathOperationKeys = [];
 document.querySelectorAll('.math-oper').forEach(el => { mathOperationKeys.push(el.textContent) })
-//Clearing one and all ["C", "DEL"]
+//Elements clearing one and all ["C", "DEL"]
 let clearOperationKeys = [];
 document.querySelectorAll('.clear-oper').forEach(el => { clearOperationKeys.push(el.textContent) })
 
-//Functions for button operations
+//Helpers functions
+const switchCaseMainCalculation = (sign) => {
+  switch (sign) {
+    case "+":
+      lastOperationNumber = Number(lastOperationNumber) + Number(nextOperationNumber);
+      break;
+    case "-":
+      lastOperationNumber = Number(lastOperationNumber) - Number(nextOperationNumber)
+      break;
+    case "*":
+      lastOperationNumber = Number(lastOperationNumber) * Number(nextOperationNumber)
+      break;
+    case "/":
+      if (Number(nextOperationNumber) === 0) {
+        lastOperationNumber = 'Error DIV/0!'
+        calcResultDisplay.textContent = lastOperationNumber;
+      } else {
+        lastOperationNumber = Number(lastOperationNumber) / Number(nextOperationNumber)
+      }
+      break;
+  }
+}
+
+//Clear/delete functions for clear button operations
 const allClear = () => {
   lastOperationNumber = '';
   nextOperationNumber = '';
@@ -38,13 +61,13 @@ const allClear = () => {
 const deleteOneNum = () => {
   if (calcResultDisplay.textContent.length > 1) {
     //del last digit on display
-    //if we typing digits first time
+    //if we typing digits first time or don't have second number yet
     if (nextOperationNumber === "") {
       if (lastOperationNumber.length > 1) {
         lastOperationNumber = lastOperationNumber.slice(0, -1);
         calcResultDisplay.textContent = calcResultDisplay.textContent.slice(0, -1);
 
-        console.log(lastOperationNumber, "del", typeof nextOperationNumber)
+        console.log(lastOperationNumber, "del")
       }
     }
     //if we already have one number to calculate
@@ -52,16 +75,11 @@ const deleteOneNum = () => {
       nextOperationNumber = nextOperationNumber.slice(0, -1);
       calcResultDisplay.textContent = calcResultDisplay.textContent.slice(0, -1);
 
-      console.log(nextOperationNumber, "del", typeof nextOperationNumber)
+      console.log(nextOperationNumber, "del")
 
     }
   }
 }
-const percent = () => {
-  operationSign = '%';
-}
-
-
 
 //EventListeners
 document.querySelector('.calc-operations').addEventListener('click', (event) => {
@@ -96,81 +114,71 @@ document.querySelector('.calc-operations').addEventListener('click', (event) => 
       calcResultDisplay.textContent = nextOperationNumber;
       // calcOperationsChainDisplay.textContent = `${lastOperationNumber}${operationSign}`;
     }
-    console.log(lastOperationNumber, nextOperationNumber, operationSign, 'digit press');
+    console.log("a=", lastOperationNumber, "b=", nextOperationNumber, operationSign, 'digit press');
     return;
   }
 
   //if pressed operation +-/* etc. 
   if (mathOperationKeys.includes(key)) {
     //CALCULATING before "=" 
-    //(if we select two numbers and click on action) woking 1 time(because !== "=")
-    if (operationSign && nextOperationNumber === "") {
-      switch (operationSign) {
-        case "+":
-          lastOperationNumber = Number(lastOperationNumber) + Number(nextOperationNumber);
-          break;
-        case "-":
-          lastOperationNumber = Number(lastOperationNumber) - Number(nextOperationNumber)
-          break;
-        case "*":
-          lastOperationNumber = Number(lastOperationNumber) * Number(nextOperationNumber)
-          break;
-        case "/":
-          if (Number(nextOperationNumber) === 0) {
-            lastOperationNumber = 'Error DIV/0!'
-            calcResultDisplay.textContent = lastOperationNumber;
-          } else {
-            lastOperationNumber = Number(lastOperationNumber) / Number(nextOperationNumber)
-          }
-          break;
-      }
+    //(if we select one numbers and click on action)
+    if (operationSign !== "" && nextOperationNumber === "") {
+      //collection session store data
+      currentOperationsSessionStore.push(lastOperationNumber, operationSign, nextOperationNumber);
+
+
+      switchCaseMainCalculation(operationSign);
+
       calcOperationsChainDisplay.textContent = `${lastOperationNumber}${operationSign}${nextOperationNumber}`;
+
+      //Collection operations you did
+      if (lastOperationNumber && nextOperationNumber) {
+        currentOperationsSessionStore.push("=", lastOperationNumber);
+        resultsStore.push(currentOperationsSessionStore.join(""));
+      }
+      currentOperationsSessionStore = [];
+      console.log(resultsStore, currentOperationsSessionStore);
     }
     operationSign = key;
     calcOperationsChainDisplay.textContent = `${lastOperationNumber}${operationSign}`;
-    console.log(lastOperationNumber, nextOperationNumber, operationSign, "oper press");
+    console.log("a=", lastOperationNumber, "b=", nextOperationNumber, operationSign, "oper press");
     return;
   }
 
   //Calculation After pressing Result btn "="
   if (key === '=') {
+    if (lastOperationNumber === "" && operationSign === "") {
+      return;
+    }
+    if (lastOperationNumber === "" && nextOperationNumber === "") {
+      return;
+    }
+
     calcOperationsChainDisplay.textContent = `${lastOperationNumber}${operationSign}${nextOperationNumber}`;
-    currentOperationsSessionStore.push(lastOperationNumber, operationSign, nextOperationNumber);
 
     //Only one number entered, we'll count with the same numbers
-    if (nextOperationNumber === '') {
+    if (nextOperationNumber === '' && operationSign !== "") {
       nextOperationNumber = Number(lastOperationNumber);
       calcOperationsChainDisplay.textContent = `${lastOperationNumber}${operationSign}${nextOperationNumber}`;
     };
 
-    //counting logic
-    switch (operationSign) {
-      case "+":
-        lastOperationNumber = Number(lastOperationNumber) + Number(nextOperationNumber);
-        break;
-      case "-":
-        lastOperationNumber = Number(lastOperationNumber) - Number(nextOperationNumber)
-        break;
-      case "*":
-        lastOperationNumber = Number(lastOperationNumber) * Number(nextOperationNumber)
-        break;
-      case "/":
-        if (Number(nextOperationNumber) === 0) {
-          lastOperationNumber = 'Error DIV/0!'
-          calcResultDisplay.textContent = lastOperationNumber;
-        } else {
-          lastOperationNumber = Number(lastOperationNumber) / Number(nextOperationNumber)
-        }
-        break;
-    }
+    //collection session store data
+    currentOperationsSessionStore.push(lastOperationNumber, operationSign, nextOperationNumber);
+
+    //Counting logic !
+    switchCaseMainCalculation(operationSign);
+
     finish = true;
     calcResultDisplay.textContent = lastOperationNumber;
-    console.log(lastOperationNumber, nextOperationNumber, operationSign, 'result press')
+    console.log("a=", lastOperationNumber, "b=", nextOperationNumber, operationSign, 'result press')
+
+    //Collection operations you did
+    if (lastOperationNumber && nextOperationNumber) {
+      currentOperationsSessionStore.push("=", lastOperationNumber);
+      resultsStore.push(currentOperationsSessionStore.join(""));
+    }
+    currentOperationsSessionStore = [];
+    console.log(resultsStore, currentOperationsSessionStore);
   }
-  //Collection operations you did
-  currentOperationsSessionStore.push("=", lastOperationNumber);
-  resultsStore.push(currentOperationsSessionStore.join(""));
-  currentOperationsSessionStore = [];
-  console.log(resultsStore);
 
 })
